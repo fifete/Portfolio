1e57d7e55a9136734f36387c8768d7d3a5c652cd
day all <danielafuentesdev@gmail.com>
2022-11-08 11:15:16 -0500

    feat: add projects data file

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const data = {
+  projects: [
+    {
+      id: 1,
+      Info: {
+        title: 'KittyChat',
+        src: 'kingMax',
+        github: 'https://github.com/fifete/kittychat-frontend',
+        demo: 'https://voluble-custard-29d29e.netlify.app/'
+      },
+      modal: {
+        title: 'KittyChat',
+        rol: 'Frontend Developer | Designer UX',
+        description: `kitty chat 😸 is an instant 📲 messaging application where you can create channels or join them to connect with your friends 👭👩🏼‍🤝‍🧑🏽.`
+      }
+    },
+    {
+      id: 2,
+      Info: {
+        title: 'A+Aventuras',
+        src: 'a+Aventuras',
+        github: 'https://github.com/fifete/turist-social-network',
+        demo: 'https://fifete.github.io/turist-social-network/'
+      },
+      modal: {
+        title: 'A+Aventuras',
+        rol: 'Frontend Developer',
+        description: `Red social 👨‍👩‍👧‍👧 donde el usuario pueda promocionar el turismo de diferentes partes del mundo 🌎, al redactar su experiencia 📝 y publicar una foto 📷 del lugar.`
+      }
+    }
+  ],
+  technologies: [
+    {
+      name: 'Figma',
+      src: 'https://img.icons8.com/office/45/000000/figma.png',
+      projects: [1, 2]
+    },
+    {
+      name: 'Firebase',
+      src: 'https://img.icons8.com/color/48/000000/firebase.png',
+      projects: [2]
+    },
+  ],
+};
