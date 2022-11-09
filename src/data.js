18b9fbb9a48f52f050224b6ebadf3d3883361ec1
day all <danielafuentesdev@gmail.com>
2022-11-09 10:02:54 -0500

    feat: add proyect data

--- src/data.js
+++ src/data.js
@@ -25,20 +25,108 @@ export const data = {
       modal: {
         title: 'A+Aventuras',
         rol: 'Frontend Developer',
-        description: `Red social 👨‍👩‍👧‍👧 donde el usuario pueda promocionar el turismo de diferentes partes del mundo 🌎, al redactar su experiencia 📝 y publicar una foto 📷 del lugar.`
+        description: `Social network 👨‍👩‍👧‍👧 where the user can promote tourism from different parts of the world 🌎, by writing their experience 📝 and posting a photo 📷 of the place.`
+      }
+    },
+    {
+      id: 3,
+      basic: {
+        title: 'Burgerqueen',
+        src: 'burguerQueen',
+        github: 'https://github.com/fifete/LIM018-burger-queen',
+        demo: 'https://burguerqueens.netlify.app/'
+      },
+      modal: {
+        title: 'Burgerqueen',
+        rol: 'Frontend Developer | Designer UI & UX',
+        description: `Burger Queen 🍔 🍟 is an order management platform, focused on users with the following roles: 👩 Administrator, 🤵 Waiter and 👩‍🍳 Chef.`
+      }
+    },
+    {
+      id: 4,
+      basic: {
+        title: 'Studio Ghibli',
+        src: 'studioGhibli',
+        github: 'https://github.com/fifete/studio-ghibli',
+        demo: 'https://fifete.github.io/studio-ghibli/'
+      },
+      modal: {
+        title: 'Studio Ghibli',
+        rol: 'Frontend Developer | Designer UI & UX',
+        description: `Information about the movies 🎥 of the animated studio Studio Ghibli that can be filtered and sorted 📂`
+      }
+    },
+    {
+      id: 5,
+      basic: {
+        title: 'KingMax',
+        src: 'kingMax',
+        github: 'https://github.com/fifete/Card-validation',
+        demo: 'https://fifete.github.io/Card-validation/'
+      },
+      modal: {
+        title: 'KingMax',
+        rol: 'Frontend Developer | Designer UI & UX',
+        description: `Validation of the card number 💳 to make the payment of the service subscription 🎦 KINGMAX`
       }
     }
   ],
   technologies: [
     {
-      name: 'Figma',
-      src: 'https://img.icons8.com/office/45/000000/figma.png',
-      projects: [1, 2]
+      name: 'ReactJS',
+      src: 'https://img.icons8.com/color/48/000000/react-native.png',
+      projects: [1]
+    },
+    {
+      name: 'Angular',
+      src: 'https://img.icons8.com/color/48/000000/firebase.png',
+      projects: [3]
+    },
+    {
+      name: 'JavaScript',
+      src: 'https://img.icons8.com/color/48/000000/javascript.png',
+      projects: [2, 4, 5]
+    },
+    {
+      name: 'Typescript',
+      src: 'https://img.icons8.com/color/48/000000/typescript.png',
+      projects: [3]
+    },
+    {
+      name: 'HTML5',
+      src: 'https://img.icons8.com/color/48/000000/html-5.png',
+      projects: [2, 4, 5]
+    },
+    {
+      name: 'CSS3',
+      src: 'https://img.icons8.com/color/48/000000/css3.png',
+      projects: [1, 2, 3, 4, 5]
     },
     {
       name: 'Firebase',
       src: 'https://img.icons8.com/color/48/000000/firebase.png',
-      projects: [2]
+      projects: [2, 3]
+    },
+    {
+      name: 'PostgreSQL',
+      src: 'https://img.icons8.com/color/48/000000/postgresql.png',
+      projects: [1]
+    },
+    {
+      name: 'Jasmine',
+      src: '%PUBLIC_URL%/assets/images/tech/jasmine.png',
+      projects: [3]
+    },
+    {
+      name: 'Jest',
+      src: '%PUBLIC_URL%/assets/images/tech/jest.png',
+      projects: [2, 4, 5]
     },
   ],
 };
+
+/* 
+ReactJS+ CSS3 + socket.io + Express.js + Git + Postgresql + SQL
+ Html5 + CSS3 + Typescript+ Angular + Git + Jasmine + Firebase
+ Html5 + CSS3 + JavaScript + Firebase+ Jest + Git
+ */
